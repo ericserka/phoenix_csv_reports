246fdcc2dc4b643ecff0cfe45118dae6ad61f8b9
ericserka <ericserka@hotmail.com>
2023-03-04 13:56:16 -0300

    feat: adding cors plug

--- lib/phoenix_csv_reports_web/endpoint.ex
+++ lib/phoenix_csv_reports_web/endpoint.ex
@@ -41,5 +41,6 @@ defmodule PhoenixCsvReportsWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug
   plug PhoenixCsvReportsWeb.Router
 end
--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule PhoenixCsvReports.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tarams, "~> 1.7"}
+      {:tarams, "~> 1.7"},
+      {:cors_plug, "~> 3.0"}
     ]
   end
 
