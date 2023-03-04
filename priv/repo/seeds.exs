21eba1026d019d141a3df11c9933510792886a15
ericserka <ericserka@hotmail.com>
2023-03-04 15:29:20 -0300

    fix: registrations with different days in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,7 +10,6 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias PhoenixCsvReports.Reports
 alias PhoenixCsvReports.Reports.{Partner, Registration}
 alias PhoenixCsvReports.Repo
 
@@ -18,16 +17,30 @@ Repo.delete_all(Registration)
 Repo.delete_all(Partner)
 
 partners =
-  for i <- 1..5 do
-    {:ok, partner} = Reports.create_partner(%{name: "Partner #{i}"})
-    partner
-  end
+  Enum.map(
+    1..5,
+    &(%Partner{}
+      |> Partner.changeset(%{name: "Partner #{&1}"})
+      |> Repo.insert!())
+  )
 
-for i <- 1..100 do
-  Reports.create_registration(%{
-    name: "Registration #{i}",
-    cpf: Brcpfcnpj.cpf_generate(),
-    email: "email#{i}@hotmail.com",
-    partner_id: partners |> Enum.random() |> Map.get(:id)
-  })
-end
+dates_range = Date.range(~D[2022-05-20], ~D[2023-05-20])
+
+Repo.insert_all(
+  Registration,
+  Enum.map(
+    1..1000,
+    fn i ->
+      date = dates_range |> Enum.random() |> NaiveDateTime.new!(~T[00:00:00])
+
+      %{
+        name: "Registration #{i}",
+        cpf: Brcpfcnpj.cpf_generate(),
+        email: "email#{i}@hotmail.com",
+        partner_id: partners |> Enum.random() |> Map.get(:id),
+        inserted_at: date,
+        updated_at: date
+      }
+    end
+  )
+)
