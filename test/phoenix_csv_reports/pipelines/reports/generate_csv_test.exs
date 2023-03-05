651f130cdd350b1521bd3692515f270f8ec630ec
ericserka <ericserka@hotmail.com>
2023-03-04 22:24:18 -0300

    test: adding generate_csv_test file

---
+++ test/phoenix_csv_reports/pipelines/reports/generate_csv_test.exs
@@ -0,0 +1,9 @@
+defmodule PhoenixCsvReports.Pipelines.Reports.GenerateCsvTest do
+  use PhoenixCsvReports.DataCase
+  import PhoenixCsvReports.ReportsFixtures
+
+  test "test" do
+    registration = registration_fixture()
+    assert true
+  end
+end
